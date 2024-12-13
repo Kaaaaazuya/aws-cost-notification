0e58509679908f0c4225dbd018fc9aabc964f6c0
Kazuya Yokoyama <yk3kzy@gmail.com>
2024-12-14 08:14:00 +0900

    コスト計算処理の実装

---
+++ lambda/notification/cost/explorer.go
@@ -0,0 +1,46 @@
+package cost
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/costexplorer"
+	"github.com/aws/aws-sdk-go-v2/service/costexplorer/types"
+)
+
+type explorerClient interface {
+	GetCostAndUsage(ctx context.Context, params *costexplorer.GetCostAndUsageInput, optFns ...func(*costexplorer.Options)) (*costexplorer.GetCostAndUsageOutput, error)
+}
+
+// NewExplorerClient returns a new cost explorer client
+func NewExplorerClient(region string) (*costexplorer.Client, error) {
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithRegion(region),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	client := costexplorer.NewFromConfig(cfg)
+	return client, nil
+}
+
+// GetCostAndUsage returns the cost for the current month
+func GetCostAndUsage(ctx context.Context, client explorerClient, start *string, end *string) (*costexplorer.GetCostAndUsageOutput, error) {
+	return client.GetCostAndUsage(ctx, createGetCostAndUsageInput(start, end))
+}
+
+
+// createGetCostAndUsageInput returns the input for the cost explorer
+func createGetCostAndUsageInput(start *string, end *string) *costexplorer.GetCostAndUsageInput {
+	return &costexplorer.GetCostAndUsageInput{
+		Granularity: "MONTHLY",
+		Metrics: []string{
+			"UnblendedCost",
+		},
+		TimePeriod: &types.DateInterval{
+			Start: start,
+			End:   end,
+		},
+	}
+}
--- lambda/notification/main.go
+++ lambda/notification/main.go
@@ -1,7 +1,63 @@
 package notification
 
-import "fmt"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"strconv"
+	"time"
+
+	"github.com/Kaaaaazuya/aws-cost-notification/cost"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/service/costexplorer"
+)
+
+// createTimePeriod returns the start and end date of the current month
+func createTimePeriod() (start, end *string) {
+	today := time.Now()
+	mf := time.Date(today.Year(), today.Month(), 1, 0, 0, 0,0, time.UTC)
+	me := mf.AddDate(0, 0, -1)
+
+	mfs := mf.Format("2006-01-02")
+	mes := me.Format("2006-01-02")
+	return &mfs, &mes
+}
+
+func sumCost(cost *costexplorer.GetCostAndUsageOutput) (total string){
+	sum := 0.0
+	for _, data := range cost.ResultsByTime[0].Groups {
+		amount, _ := strconv.ParseFloat(*data.Metrics["UnblendedCost"].Amount, 64)
+		sum = sum + amount
+	}
+	total = fmt.Sprintf("%.10f", sum)
+	return total
+}
+
+func  handleRequest(ctx context.Context, event json.RawMessage) error {
+	log.Println("コスト取得バッチ 開始")
+	c, err := cost.NewExplorerClient("ap-northeast-1")
+	if err != nil {
+		return err
+	}
+
+	s, e := createTimePeriod()
+	log.Println("start:", *s, "end:", *e)
+	cost, err := cost.GetCostAndUsage(context.TODO(), c, s, e)
+	if err != nil {
+		return err
+	}
+
+	log.Println(cost)
+	total := sumCost(cost)
+	log.Println("合計コスト:", total)
+	log.Println("コスト取得バッチ 終了")
+	return nil
+}
+
+
 
 func main() {
-	fmt.Print("hello world")
+	lambda.Start(handleRequest)
 }
+
